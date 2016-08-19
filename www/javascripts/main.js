100735060c0a0bb20254f6bd64db9a5f7599278e
Max Amey <maxwellamey@gmail.com>
2016-08-19 15:42:37 -0500

    Added the ability to click the circle and make it green to show that the task is complete. Still need to add the strikethrough to the text when the circle is green.

--- www/javascripts/main.js
+++ www/javascripts/main.js
@@ -7,13 +7,15 @@ $(function(){
   var $todoTextEl = $("[data-js='todo__textInput']");
   var $todoEnterEl = $("[data-js='todo__enter']");
   var $listEl = $("[data-js='list']");
+  var $listTextEl = $("[data-js='list__text']");
   var $list__itemEl = $("[data-js='list__item']");
+  var $circle = $("[data-js='circle']");
 
-  $(document).keyup(function(e){
-    if(keyCode == 46){
-      e.removeClass("list__item--selected");
-    };
-  });
+  // $(document).keyup(function(e){
+  //   if(keyCode == 46){
+  //     e.removeClass("list__item--selected");
+  //   };
+  // });
 
   $todoEnterEl.on("click", function(e){
     e.preventDefault();
@@ -24,19 +26,30 @@ $(function(){
 
   function submitTodo(submitString){
     var template = `
-      <li class="list__item"
-         data-js="list__item">
-         ${submitString}
-      </li>
+    <li class="list__item"
+       data-js="list__item">
+       <div class="circle"
+            data-js="circle">
+       </div>
+       <p class="list__text"
+          data-js="list__text">
+        ${submitString}
+       </p>
+    </li>
     `;
     if($todoTextEl.val().length >= 2){
       $listEl.append(template);
     };
   };
 
-  $listEl.on("click", "[data-js='list__item']", function(e){
+  $listEl.on("click", "[data-js='list__text']", function(e){
     var $selectedItem = $(e.currentTarget);
-    $selectedItem.toggleClass("list__item--selected");
+    $selectedItem.parent().toggleClass("list__item--selected");
+  });
+
+  $listEl.on("click", "[data-js='circle']", function(e){
+    var $circleSelected = $(e.currentTarget);
+    $circleSelected.toggleClass("circle__selected");
   });
 
 
